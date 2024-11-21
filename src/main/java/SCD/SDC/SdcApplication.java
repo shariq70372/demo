b8b11e9edddb6d4d5add11d6676f1356a86b6f94
local <mstudent@ELAB206-25.iqra.edu.pk>
2024-11-21 15:49:35 +0500

    Initial commit

---
+++ src/main/java/SCD/SDC/SdcApplication.java
@@ -0,0 +1,13 @@
+package SCD.SDC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SCD/SDC/SdcApplicationTests.java
@@ -0,0 +1,13 @@
+package SCD.SDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
