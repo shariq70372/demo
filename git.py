34a3cec904be359d430ea8bd8cd6f45a29ba475d
WajahatAfzal <wajahatafzal.qazi@gmail.com>
2024-11-28 02:12:22 +0500

    Wajahat-60263

---
+++ git.py
@@ -0,0 +1,29 @@
+print ("hello world!")
+
+# Run the main function
+
+def main():
+    print("This is the main function.")
+
+main()
+
+# Define a function
+
+def greet_user(name):
+    print("Hello " + name + "!")
+
+greet_user("Wajahat Afzal")
+
+# Declare a variable
+
+age = 30
+
+# Print the variable
+
+print("Your age is: " + str(age))
+
+# Use a loop
+
+
+
+
